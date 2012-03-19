ab746aefd2593464408737931ebb6e84cb388441
Jonas Gehring <jonas.gehring@boolsoft.org>
2012-03-20 00:06:51 +0100

    Updated example configuration

--- config.lua
+++ config.lua
@@ -1,15 +1,16 @@
 --[[
-	Example configuration for peprsrv
+	Example configuration file for peprsrv
 --]]
 
 
 local options = {
 
--- General options
-port = 8080,         -- Default port is 8080
-host = "0.0.0.0",    -- Accept connections from everywhere
-type = "svg",        -- Generate SVG images by default
-["check-head"] = 60,     -- Check for new repository head after 60 seconds
+-- General options (can be overridden by command-line arguments)
+port = 8080,              -- Default port is 8080
+host = "0.0.0.0",         -- Accept connections from everywhere
+["show-index"] = false,   -- Don't show the index page
+type = "svg",             -- Generate SVG images by default
+["check-head"] = 60,      -- Check for new repository head after 60 seconds
 
 -- Report definitions with report-specific options
 reports = {
@@ -19,14 +20,14 @@ reports = {
 		tags = "^v",            -- Show tags starting with 'v'
 	},
 
-	loc = {                     -- No extra options for /r/loc
-	},
-
 	filetypes_pie = {           -- /r/filetypes
 		_path = "filetypes",    -- Run the "filetypes" report
 		type = "png",           -- Generate a PNG image for this report
 		size = "620x500",       -- Image size
 		pie = true,             -- Generate a pie chart
+	},
+
+	loc = {                     -- No extra options for /r/loc
 	},
 
 	-- These are the other graphical reports, without any extra options
@@ -43,4 +44,4 @@ reports = {
 
 } -- End of options
 
-return options
+return options -- Don't forget this
