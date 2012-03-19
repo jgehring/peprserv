e773c8285a86943c6b1a501721db5272d65b00fc
Jonas Gehring <jonas.gehring@boolsoft.org>
2012-03-19 22:12:00 +0100

    Added all graphical reports to example configuration

--- config.lua
+++ config.lua
@@ -9,7 +9,7 @@ local options = {
 port = 8080,         -- Default port is 8080
 host = "0.0.0.0",    -- Accept connections from everywhere
 type = "svg",        -- Generate SVG images by default
-check-head = 60,     -- Check for new repository head after 60 seconds
+["check-head"] = 60,     -- Check for new repository head after 60 seconds
 
 -- Report definitions with report-specific options
 reports = {
@@ -22,11 +22,23 @@ reports = {
 	loc = {                     -- No extra options for /r/loc
 	},
 
-	filetypes = {               -- /r/filetypes
+	filetypes_pie = {           -- /r/filetypes
+		_path = "filetypes",    -- Run the "filetypes" report
 		type = "png",           -- Generate a PNG image for this report
-		size = "500x500",       -- Image size
+		size = "620x500",       -- Image size
 		pie = true,             -- Generate a pie chart
-	}
+	},
+
+	-- These are the other graphical reports, without any extra options
+	activity = { },
+	authors_pie = { },
+	commit_counts = { },
+	directories = { },
+	filetypes = { },
+	-- participation = { }, -- disabled, as author argument is mandatory
+	punchcard = { },
+	times = { },
+	volume = { },
 },
 
 } -- End of options
